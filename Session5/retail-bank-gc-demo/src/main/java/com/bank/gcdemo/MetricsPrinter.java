a0513c6a92968425a0f0b6bd22060196ee2974c8
manojkumarsingh77 <manojsc99@gmail.com>
2026-05-27 11:24:48 +0530

    updated files

--- Session5/retail-bank-gc-demo/src/main/java/com/bank/gcdemo/MetricsPrinter.java
+++ Session5/retail-bank-gc-demo/src/main/java/com/bank/gcdemo/MetricsPrinter.java
@@ -1,4 +1,29 @@
 package com.bank.gcdemo;
 
 public class MetricsPrinter {
+
+    public void printMetrics(long txnsProcessed, long startTime) {
+
+        Runtime runtime = Runtime.getRuntime();
+
+        long usedHeapMb =
+                (runtime.totalMemory() - runtime.freeMemory()) / (1024 * 1024);
+
+        long totalHeapMb =
+                runtime.totalMemory() / (1024 * 1024);
+
+        long maxHeapMb =
+                runtime.maxMemory() / (1024 * 1024);
+
+        long uptimeSeconds =
+                (System.currentTimeMillis() - startTime) / 1000;
+
+        System.out.println(
+                "Processed=" + txnsProcessed +
+                        " | UsedHeapMB=" + usedHeapMb +
+                        " | TotalHeapMB=" + totalHeapMb +
+                        " | MaxHeapMB=" + maxHeapMb +
+                        " | UptimeSec=" + uptimeSeconds
+        );
+    }
 }
